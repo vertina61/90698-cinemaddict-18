37af9ec4b1edcf9f118f3405b3f6295fac1ed5e8
vertina61 <natalya.vertieva@mail.ru>
2022-09-18 14:06:42 +0300

    Правки по комментариям

--- src/mock/comment.js
+++ src/mock/comment.js
@@ -9,19 +9,20 @@ const generateComment = ()=>({
   emotion: getRandomValue(emotions),
 });
 
-const getCommentCount = (films)=>films.reduce(
+const getCommentCount = (films) => films.reduce(
   (count, film)=>count + film.comments.length, 0
 );
 
 const generateComments = (films)=>{
   const commentCount = getCommentCount(films);
-  return Array.from({length: commentCount}, (_value, index)=>{
+  return Array.from({length: commentCount}, (_value, index) => {
     const commentItem = generateComment();
     return {
       id: String(index + 1),
-      commentItem,
+      ...commentItem,
     };
   });
 };
 
 export {generateComments};
+
