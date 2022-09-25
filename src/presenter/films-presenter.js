7143594ac98da770e8308797148b7fb1e6d9c50d
vertina61 <natalya.vertieva@mail.ru>
2022-09-25 15:57:30 +0300

    3.12. Революция или эволюция? (часть 2) с ошибками

--- src/presenter/films-presenter.js
+++ src/presenter/films-presenter.js
@@ -5,12 +5,13 @@ import FilmsListView from '../view/films-list-view.js';
 import FilmsListContainerView from '../view/films-list-container-view.js';
 import FilmCardPopupView from '../view/film-card-popup-view.js';
 import {render} from '../render.js';
+import { FILM_COUNT_PER_STEP, FILM_COUNT} from '../mock/const.js';
 
 export default class FilmsPresenter {
   #filmsComponent = new FilmsView();
   #filmsListComponent = new FilmsListView();
   #filmsListContainer = new FilmsListContainerView();
-  #ShowMoreView = new ShowMoreView ();
+  #showMoreView = new ShowMoreView ();
   #filmCardPopupComponent = null;
 
   #filmsContainer = null;
@@ -18,26 +19,29 @@ export default class FilmsPresenter {
   #commentsModel = null;
 
   #films = [];
+  #renderedFilmCount = FILM_COUNT_PER_STEP;
 
-  init(filmsContainer, filmsModel, commentsModel) {
+  constructor(filmsContainer, filmsModel, commentsModel) {
     this.#filmsContainer = filmsContainer;
     this.#filmsModel = filmsModel;
     this.#commentsModel = commentsModel;
+  }
 
+  init = () => {
     this.#films = [...this.#filmsModel.get()];
-
-    render(this.#filmsComponent, this.#filmsContainer);
     render(this.#filmsListComponent, this.#filmsComponent.element);
     render(this.#filmsListContainer, this.#filmsListComponent.element);
 
-    this.#films.forEach((film) => {
-      this.#renderFilm(film, this.#filmsListContainer);
+    this.#films.slice(0, this.#renderedFilmCount).forEach((film) => {
+      this.#renderFilm(film, this.#filmsListContainer.element);
     });
+    if (this.#renderedFilmCount < FILM_COUNT) {
+      render(this.#showMoreView, this.#filmsListComponent.element);
+      this.#showMoreView.element.addEventListener('click', this.#onShowFilmButtonMore);
+    }
+  };
 
-    render(this.#ShowMoreView, this.#filmsListComponent.element);
-  }
-
-  #renderFilm(film, filmsContainer) {
+  #renderFilm(film, container) {
     const filmCardViewComponent = new FilmCardView(film);
     const linkFilmCardElement = filmCardViewComponent.element.querySelector('a');
     linkFilmCardElement.addEventListener('click', () => {
@@ -45,7 +49,7 @@ export default class FilmsPresenter {
       document.addEventListener('keydown', this.#onEscKeyDown);
     });
 
-    render(filmCardViewComponent, filmsContainer.element);
+    render(filmCardViewComponent, container);
   }
 
   #renderFilmCardPopup(film) {
@@ -78,5 +82,23 @@ export default class FilmsPresenter {
       document.removeEventListener('keydown', this.#onEscKeyDown);
     }
   };
+
+  #onShowFilmButtonMore = (evt)=> {
+    evt.preventDefault();
+
+    this.#films
+      .slice(this.#renderedFilmCount, this.#renderedFilmCount + FILM_COUNT_PER_STEP)
+      .forEach((film) => {
+        this.#renderFilm(film, this.#filmsListContainer.element);
+      });
+
+    this.#renderedFilmCount += FILM_COUNT_PER_STEP;
+
+    if (this.#renderedFilmCount >= this.#films.length) {
+      this.#showMoreView.element.remove();
+      this.#showMoreView.removeElement();
+    }
+  };
 }
+
 
